1fb8f062ebdaf39e0f19381a3c8db8e98c138b39
vladimirsaakian <31295797+vladimirsaakian@users.noreply.github.com>
2018-02-06 12:12:34 -0800

    “message”

---
+++ keys.js
@@ -0,0 +1,12 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 	'Bu3zjgXEWtdfFDqC4lSpw8jXg',
+  consumer_secret: 'dCyo492uttDV7ed8TPhDPUBiI9vq5k3TEEMvZdPtfDWbRouzzA',
+  access_token_key: '	960964139173560321-Wxwzr0TG95FkZnLx7w2sXtSkuxdPp5I',
+  access_token: '2167471745-k4BfZLXGlYgcFKosTepoi5XrgJ351AAnw1eP9wS',
+  access_token_secret: '	Cb0M87cJnJ3HJLvkiwSo2OW4IqBfo28uz4wdFifz1ZlZe',
+
+});
+
+module.exports = client
